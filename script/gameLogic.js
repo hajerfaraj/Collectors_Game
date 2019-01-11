9e69df00bb353f5010aaa16a131263173be22de6
IslamG <IslamG@github.com>
2019-01-11 17:37:25 +0200

    animated item when found, fixed  winCheck logic

--- script/gameLogic.js
+++ script/gameLogic.js
@@ -1,16 +1,26 @@
 //called when image is clicked to hide img WIP
 function found(item){
-    
     var  name=item.src;
     var ul=document.getElementById('itemList');
     var list=ul.getElementsByTagName('li');
+    //remove name from list, and show img
 		for (var j=0;j<list.length; j++){
             if ( name.indexOf( list[j].innerText ) > -1 ) {
-                alert("Found");
-                list[j].style="text-decoration: line-through;filter: contrast(100%) brightness(100%)!important;";
+                list[j].style="text-decoration: line-through;";
+                list[j].firstChild.style="filter: contrast(100%) brightness(100%)!important;";
                 break;
             } 
-        }item.style="Display:none;";
+        }
+    //animate found item
+        item.style="";
+        item.classList.toggle("wonItem");
+        var timeOut = setTimeout(function() {
+          item.style="Display:none;"; 
+        }, 1000);
+        timeOut = setTimeout(function() {
+          winCheck();
+        }, 1300);
+        
 }
 function timer(){
 //TODO timer 
@@ -19,8 +29,20 @@ function restartButton(){
 //TODO
 }
 function winCheck(){
-//TODO
+    var ul=document.getElementById('itemList');
+    var list=ul.getElementsByTagName('li');
+//loop through item list, comparing if struck out (found)  
+    for (var j=0;j<list.length;j++){
+            if ( list[j].style.textDecoration!="line-through" ) {
+                break;
+            }
+            else{
+                if(j==list.length-1)
+                    won();
+            }     
+        }
 }
-function won(){
 //TODO
+function won(){
+    alert("won");
 }
