96f0f6ff9c59b438514611db1ef6f3686417a956
hiba <ali93hiba@github.com>
2019-01-22 19:44:15 +0200

    add template with javascript and css

---
+++ script/templateScript.js
@@ -0,0 +1,13 @@
+$("document").ready(function(){
+$("body").append("<div></div>");
+$("div").append("<nav></nav>");
+$("nav").append("<a id='title' href='#'>Collectors</a>");
+$("nav").append("<ul></ul>");
+$("ul").append("<li id='list'></li>");
+$("li").append("<a href='#' >home</a>");
+$("li").append("<a href='#' >about</a>");
+$("li").append("<a href='#' >play now</a>");
+$("li").append("<a id='log' href='#'>login</a>");
+$("li").append("<a href='#' >register</a>");
+$("div").append("</nav");
+});
