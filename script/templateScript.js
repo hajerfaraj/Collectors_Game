79df56146a15b1bb669d8eacec5d982e283e1c73
hiba <ali93hiba@github.com>
2019-01-23 19:49:03 +0200

    add classes and id to template tags

--- script/templateScript.js
+++ script/templateScript.js
@@ -1,13 +1,13 @@
 $("document").ready(function(){
-$("body").append("<div></div>");
-$("div").append("<nav></nav>");
-$("nav").append("<a id='title' href='#'>Collectors</a>");
-$("nav").append("<ul></ul>");
-$("ul").append("<li id='list'></li>");
-$("li").append("<a href='#' >home</a>");
-$("li").append("<a href='#' >about</a>");
-$("li").append("<a href='#' >play now</a>");
-$("li").append("<a id='log' href='#'>login</a>");
-$("li").append("<a href='#' >register</a>");
-$("div").append("</nav");
+$("body").append("<div class='tdiv'></div>");
+$("div.tdiv").append("<nav class='tnav'></nav>");
+$("nav.tnav").append("<a id='title' href='#'>Collectors</a>");
+$("nav.tnav").append("<ul class='tul'></ul>");
+$("ul.tul").append("<li class='tlist'></li>");
+$("li.tlist").append("<a id='a1' href='#' >home</a>");
+$("li.tlist").append("<a id='a2' href='#' >about</a>");
+$("li.tlist").append("<a  id='a3' href='#' >play now</a>");
+$("li.tlist").append("<a id='log' href='#'>login</a>");
+$("li.tlist").append("<a  id='a5' href='#' >register</a>");
+
 });
