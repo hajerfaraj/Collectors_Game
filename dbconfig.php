da4e43eb32cce56d535da6548d3e25aaf0a1abee
hiba <ali93hiba@github.com>
2019-02-07 10:39:45 +0200

    update dbconfig

--- dbconfig.php
+++ dbconfig.php
@@ -12,22 +12,6 @@ else
 echo 'database is connected';
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf8">
-<title></title>
-</head>
-<body>
-<?php
-$quary="select * from user";
-$result=mysqli_query($conn,$quary);
-if(!$result){
-    die("error");
-}
-?>
-
-
 <?php
 mysqli_close($conn);
 
